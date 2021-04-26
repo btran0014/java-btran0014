ac4463b0288ed85858c7e247ed5d505afe9827de
Brian Tran <btran0014@gmail.com>
2021-04-26 16:18:39 -0400

    tried to do question 1
    not working
    im bad

---
+++ ICS4U/java_exercises/question1.java
@@ -0,0 +1,12 @@
+package java_exercises;
+import java.math.*;
+import java.util.Scanner;
+
+public class question1 {
+        double Pi = Math.PI;
+        System.out.println("What is the radius? ");
+        Scanner sc = new Scanner(System.in);
+        int radius = sc.nextInt();
+        System.out.println("Circumference: " + (Math.round((2 * Pi * radius)*10))/10.0);
+        System.out.println("Area: " + (Math.round((Pi * radius*radius)*10)/10.0));
+}

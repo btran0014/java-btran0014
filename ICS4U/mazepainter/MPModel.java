0c93cdbe386ed444121e038fbb9b191b0af27701
Brian Tran <btran0014@gmail.com>
2021-06-17 22:56:21 -0400

    working random level generator

--- ICS4U/mazepainter/MPModel.java
+++ ICS4U/mazepainter/MPModel.java
@@ -11,18 +11,17 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class MPModel extends JPanel implements ActionListener {
-    //blocks and size variables
+    // blocks and size variables
     private Dimension dimension; // game dimensions
     private boolean inGame = false; // in game variable
     private final int blocksize = 24; // size of block
     private final int numofblocks = 15; // number of blocks
     private final int screensize = numofblocks * blocksize; // size of the screen in terms of blocks
 
-    
     // in game variable values
     private final int playerspeed = 8; // speed of player model
-    
-    //things printed and things needed in order to print to screen
+
+    // things printed and things needed in order to print to screen
     private final Font smallFont = new Font("Arial", Font.BOLD, 14); // font
     private int score;
     private Image player_model;
@@ -31,130 +30,94 @@ public class MPModel extends JPanel implements ActionListener {
     private int playerx, playery, playerdx, playerdy;
     private int req_dx, req_dy;
 
-    //level data (borders and blocks) in form of array
+    // level data (borders and blocks) in form of array
     private final short levelData1[] = {
-        
-        //square level
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22,  0, 
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-        
+
+            // square level
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22, 0, 0,
+            21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21,
+            0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 25, 26, 26, 26, 26,
+            26, 26, 26, 26, 26, 26, 26, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
     };
     private final short levelData2[] = {
-        //spiral level
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0, 27, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22,  0, 
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0,  0,  0, 19, 26, 26, 26, 26, 26, 26, 26, 22,  0, 21,  0,
-        0,  0,  0, 21,  0,  0,  0,  0,  0,  0,  0, 21,  0, 21,  0,
-        0,  0,  0, 21,  0, 19, 26, 26, 26, 22,  0, 21,  0, 21,  0,
-        0,  0,  0, 21,  0, 21,  0,  0,  0, 21,  0, 21,  0, 21,  0,
-        0,  0,  0, 21,  0, 21,  0, 23,  0, 21,  0, 21,  0, 21,  0,
-        0,  0,  0, 21,  0, 21,  0, 25, 26, 28,  0, 21,  0, 21,  0,
-        0,  0,  0, 21,  0, 21,  0,  0,  0,  0,  0, 21,  0, 21,  0,
-        0,  0,  0, 21,  0, 25, 26, 26, 26, 26, 26, 28,  0, 21,  0,
-        0,  0,  0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0,  0,  0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-    };
+            // spiral level
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 27, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 19, 26, 26, 26, 26, 26, 26, 26, 22, 0, 21, 0, 0, 0, 0, 21,
+            0, 0, 0, 0, 0, 0, 0, 21, 0, 21, 0, 0, 0, 0, 21, 0, 19, 26, 26, 26, 22, 0, 21, 0, 21, 0, 0, 0, 0, 21, 0, 21,
+            0, 0, 0, 21, 0, 21, 0, 21, 0, 0, 0, 0, 21, 0, 21, 0, 23, 0, 21, 0, 21, 0, 21, 0, 0, 0, 0, 21, 0, 21, 0, 25,
+            26, 28, 0, 21, 0, 21, 0, 0, 0, 0, 21, 0, 21, 0, 0, 0, 0, 0, 21, 0, 21, 0, 0, 0, 0, 21, 0, 25, 26, 26, 26,
+            26, 26, 28, 0, 21, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 25, 26, 26, 26, 26, 26, 26,
+            26, 26, 26, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0 };
     private final short levelData3[] = {
-        // zigzag level
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 
-        0, 27, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0, 
-        0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0,
-        0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21,  0, 
-        0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 30,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0     
+            // zigzag level
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 27, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28, 0, 0, 21, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 21, 0, 0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28, 0, 0, 21, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 22, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 21, 0, 0, 19, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 28, 0, 0, 21, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 25, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 26, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0
 
     };
 
     private final short levelData4[] = {
-        // ??? level
-        19, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 22,
-        17, 16, 16, 16, 16, 24, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-        25, 24, 24, 24, 28, 0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
-        0,  0,  0,  0,  0,  0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
-        19, 18, 18, 18, 18, 18, 16, 16, 16, 16, 24, 24, 24, 24, 20,
-        17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
-        17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
-        17, 16, 16, 16, 24, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
-        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 18, 18, 18, 18, 20,
-        17, 24, 24, 28, 0, 25, 24, 24, 16, 16, 16, 16, 16, 16, 20,
-        21, 0,  0,  0,  0,  0,  0,   0, 17, 16, 16, 16, 16, 16, 20,
-        17, 18, 18, 22, 0, 19, 18, 18, 16, 16, 16, 16, 16, 16, 20,
-        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-        25, 24, 24, 24, 26, 24, 24, 24, 24, 24, 24, 24, 24, 24, 28
-    };
+            // ??? level
+            19, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 22, 17, 16, 16, 16, 16, 24, 16, 16, 16, 16, 16, 16,
+            16, 16, 20, 25, 24, 24, 24, 28, 0, 17, 16, 16, 16, 16, 16, 16, 16, 20, 0, 0, 0, 0, 0, 0, 17, 16, 16, 16, 16,
+            16, 16, 16, 20, 19, 18, 18, 18, 18, 18, 16, 16, 16, 16, 24, 24, 24, 24, 20, 17, 16, 16, 16, 16, 16, 16, 16,
+            16, 20, 0, 0, 0, 0, 21, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0, 0, 0, 0, 21, 17, 16, 16, 16, 24, 16, 16,
+            16, 16, 20, 0, 0, 0, 0, 21, 17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 18, 18, 18, 18, 20, 17, 24, 24, 28, 0,
+            25, 24, 24, 16, 16, 16, 16, 16, 16, 20, 21, 0, 0, 0, 0, 0, 0, 0, 17, 16, 16, 16, 16, 16, 20, 17, 18, 18, 22,
+            0, 19, 18, 18, 16, 16, 16, 16, 16, 16, 20, 17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 17,
+            16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 25, 24, 24, 24, 26, 24, 24, 24, 24, 24, 24, 24, 24,
+            24, 28 };
 
     private final short levelData5[] = {
-        //multisquare level
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0, 19, 26, 22,  0,  0,  0, 19, 26, 22,  0,  0,  0,  0,  0,
-        0, 21,  0, 21,  0,  0,  0, 21,  0, 21,  0,  0,  0,  0,  0,
-        0, 25, 26, 16, 26, 26, 26, 16, 26, 28,  0,  0,  0,  0,  0,
-        0,  0,  0, 21,  0,  0,  0, 21,  0,  0,  0,  0,  0,  0,  0,
-        0, 19, 26, 28,  0,  0,  0, 25, 26, 22,  0,  0,  0,  0,  0,
-        0, 21,  0,  0,  0,  0,  0,  0,  0, 21,  0,  0,  0,  0,  0,
-        0, 25, 26, 26, 26, 26, 26, 26, 26, 28,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        // multisquare level
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 19, 26, 22, 0, 0, 0, 19, 26, 22, 0, 0, 0, 0, 0, 0, 21, 0,
+        21, 0, 0, 0, 21, 0, 21, 0, 0, 0, 0, 0, 0, 25, 26, 16, 26, 26, 26, 16, 26, 28, 0, 0, 0, 0, 0, 0, 0, 0, 21, 0,
+        0, 0, 21, 0, 0, 0, 0, 0, 0, 0, 0, 19, 26, 28, 0, 0, 0, 25, 26, 22, 0, 0, 0, 0, 0, 0, 21, 0, 0, 0, 0, 0, 0,
+        0, 21, 0, 0, 0, 0, 0, 0, 25, 26, 26, 26, 26, 26, 26, 26, 28, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 
-
-        
     };
 
     private short levelDatax[];
-    
+
     Random rand = new Random();
-    int levelSelector = rand.nextInt(6);
-    
-        
-    if (levelSelector == 1){
+    int levelSelector = rand.nextInt(6);{
+
+    if(levelSelector==1)
+    {
         levelDatax = levelData1;
-    }else if(levelSelector == 2){
+    }else if(levelSelector==2)
+    {
         levelDatax = levelData2;
-    }else if(levelSelector == 3){
+    }else if(levelSelector==3)
+    {
         levelDatax = levelData3;
-    }else if (levelSelector == 4){
+    }else if(levelSelector==4)
+    {
         levelDatax = levelData4;
-    }else{
+    }else
+    {
         levelDatax = levelData5;
     }
-    
-}
+
+    }
 
     private short[] screenData;
     private Timer timer;
 
-    //using constructor to run the functions
+    // using constructor to run the functions
     public MPModel() {
         importModel();
         initVariables();
@@ -162,22 +125,22 @@ public class MPModel extends JPanel implements ActionListener {
         setFocusable(true);
         initGame();
     }
-    
-    //import required images 
+
+    // import required images
     private void importModel() {
         player_model = new ImageIcon("ICS4U/mazepainter/player_model.png").getImage();
     }
 
-    //initialize the required variables for the game
+    // initialize the required variables for the game
     private void initVariables() {
         screenData = new short[numofblocks * numofblocks];
         dimension = new Dimension(400, 400);
-    
+
         timer = new Timer(0, this);
         timer.start();
     }
 
-    //move and animate character model when in game and make sure maze is working
+    // move and animate character model when in game and make sure maze is working
     private void playGame(Graphics2D g2d) {
         movePlayer();
         drawPlayer(g2d);
@@ -185,15 +148,15 @@ public class MPModel extends JPanel implements ActionListener {
 
     }
 
-    //intro screen design and text
+    // intro screen design and text
     private void introScreen(Graphics2D g2d) {
-    	String instructions = "SPACE BAR TO PLAY.";
+        String instructions = "SPACE BAR TO PLAY.";
         String instructions2 = "ARROW KEYS TO MOVE.";
         String instructions3 = "ESCAPE TO PAUSE.";
         g2d.setColor(Color.gray);
-        g2d.drawString(instructions, (screensize)/4, 150);
-        g2d.drawString(instructions2, (screensize)/4-10, 170);
-        g2d.drawString(instructions3, (screensize)/4+5, 190);
+        g2d.drawString(instructions, (screensize) / 4, 150);
+        g2d.drawString(instructions2, (screensize) / 4 - 10, 170);
+        g2d.drawString(instructions3, (screensize) / 4 + 5, 190);
     }
 
     private void printScore(Graphics2D g) {
@@ -203,7 +166,6 @@ public class MPModel extends JPanel implements ActionListener {
         g.drawString(scorestring, screensize / 2 + 96, screensize + 16);
 
     }
-
 
     // check the maze
     private void checkMaze() {
@@ -241,8 +203,7 @@ public class MPModel extends JPanel implements ActionListener {
             }
 
             if (req_dx != 0 || req_dy != 0) {
-                if (!((req_dx == -1 && req_dy == 0 && (ch & 1) != 0)
-                        || (req_dx == 1 && req_dy == 0 && (ch & 4) != 0)
+                if (!((req_dx == -1 && req_dy == 0 && (ch & 1) != 0) || (req_dx == 1 && req_dy == 0 && (ch & 4) != 0)
                         || (req_dx == 0 && req_dy == -1 && (ch & 2) != 0)
                         || (req_dx == 0 && req_dy == 1 && (ch & 8) != 0))) {
                     playerdx = req_dx;
@@ -250,29 +211,28 @@ public class MPModel extends JPanel implements ActionListener {
                 }
             }
 
-            if ((playerdx == -1 && playerdy == 0 && (ch & 1) != 0)
-                    || (playerdx == 1 && playerdy == 0 && (ch & 4) != 0)
+            if ((playerdx == -1 && playerdy == 0 && (ch & 1) != 0) || (playerdx == 1 && playerdy == 0 && (ch & 4) != 0)
                     || (playerdx == 0 && playerdy == -1 && (ch & 2) != 0)
                     || (playerdx == 0 && playerdy == 1 && (ch & 8) != 0)) {
                 playerdx = 0;
                 playerdy = 0;
             }
-        } 
+        }
         playerx = playerx + playerspeed * playerdx;
         playery = playery + playerspeed * playerdy;
     }
 
-    //print the player model according to the keystroke
+    // print the player model according to the keystroke
     private void drawPlayer(Graphics2D g2d) {
 
         if (req_dx == -1) {
-        	g2d.drawImage(player_model, playerx + 1, playery + 1, this);
+            g2d.drawImage(player_model, playerx + 1, playery + 1, this);
         } else if (req_dx == 1) {
-        	g2d.drawImage(player_model, playerx + 1, playery + 1, this);
+            g2d.drawImage(player_model, playerx + 1, playery + 1, this);
         } else if (req_dy == -1) {
-        	g2d.drawImage(player_model, playerx + 1, playery + 1, this);
+            g2d.drawImage(player_model, playerx + 1, playery + 1, this);
         } else {
-        	g2d.drawImage(player_model, playerx + 1, playery + 1, this);
+            g2d.drawImage(player_model, playerx + 1, playery + 1, this);
         }
     }
 
@@ -286,28 +246,27 @@ public class MPModel extends JPanel implements ActionListener {
             for (x = 0; x < screensize; x += blocksize) {
 
                 g2d.setColor(Color.black);
-            
-                
-                if ((levelDatax[i] == 0)) { 
-                	g2d.fillRect(x, y, blocksize, blocksize);
-                 }
 
-                if ((screenData[i] & 16) != 0) { 
+                if ((levelDatax[i] == 0)) {
+                    g2d.fillRect(x, y, blocksize, blocksize);
+                }
+
+                if ((screenData[i] & 16) != 0) {
                     g2d.setColor(Color.white);
                     g2d.fillRect(x, y, blocksize, blocksize);
-               }
+                }
                 i++;
             }
         }
     }
 
-    //initialize the game and level
+    // initialize the game and level
     private void initGame() {
         score = 0;
         initLevel();
     }
 
-    //initialize the level
+    // initialize the level
     private void initLevel() {
         int i;
         for (i = 0; i < numofblocks * numofblocks; i++) {
@@ -317,13 +276,13 @@ public class MPModel extends JPanel implements ActionListener {
         continueLevel();
     }
 
-    //direction controls and starting position
+    // direction controls and starting position
     private void continueLevel() {
-        playerx = 1 * blocksize;  //start position
+        playerx = 1 * blocksize; // start position
         playery = 1 * blocksize;
-        playerdx = 0;	//reset direction move
+        playerdx = 0; // reset direction move
         playerdy = 0;
-        req_dx = 0;		// reset direction controls
+        req_dx = 0; // reset direction controls
         req_dy = 0;
     }
 
@@ -349,11 +308,11 @@ public class MPModel extends JPanel implements ActionListener {
         g2d.dispose();
     }
 
-    //keyadapter for keystroke input
+    // keyadapter for keystroke input
     class TAdapter extends KeyAdapter {
 
         @Override
-        //player control and movement + other key strokes
+        // player control and movement + other key strokes
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
 
@@ -372,7 +331,7 @@ public class MPModel extends JPanel implements ActionListener {
                     req_dy = 1;
                 } else if (key == KeyEvent.VK_ESCAPE && timer.isRunning()) {
                     inGame = false;
-                } 
+                }
             } else {
                 if (key == KeyEvent.VK_SPACE) {
                     inGame = true;
@@ -380,10 +339,10 @@ public class MPModel extends JPanel implements ActionListener {
                 }
             }
         }
-}
+    }
 
     @Override
-    //repaint the screen when action performed (updates screen)
+    // repaint the screen when action performed (updates screen)
     public void actionPerformed(ActionEvent e) {
         repaint();
     }
